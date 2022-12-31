adeff2ab6b94c25d611c1f811cf33420e86ed9d6
JuanAndresRodriguez <juan.rodriguezpedreira@gmail.com>
2022-12-31 17:02:23 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * OTP = One-time pad
+        * Encryption technique that cannot be cracked,
+        * but requires the use of a one-time pre-shared
+        * key the same size as, or longer than, the message
+        * being sent. In this technique, plaintext is paired
+        * with a random secret key (also referred to as a one-time pad).
+        * */
+
+        EncryptionProgram ep = new EncryptionProgram();
+    }
+}
